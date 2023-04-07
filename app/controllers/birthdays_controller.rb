2bef7877f9ca86972f1540b064c07a511c98285e
Sam Wells <sfwells0518@gmail.com>
2023-04-07 11:18:59 -0500

    add update REST and update to birthdays controller

--- app/controllers/birthdays_controller.rb
+++ app/controllers/birthdays_controller.rb
@@ -21,4 +21,14 @@ class BirthdaysController < ApplicationController
     render :show
     # render json: { message: "birthday create test" }
   end
+
+  def update
+    @birthday = Birthday.find_by(id: params[:id])
+    @birthday.first_name = params[:first_name] || @birthday.first_name
+    @birthday.last_name = params[:last_name] || @birthday.last_name
+    @birthday.date_of_birth = params[:date_of_birth] || @birthday.date_of_birth
+
+    @birthday.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "/birthdays" => "birthdays#index"
   get "/birthdays/:id" => "birthdays#show"
   post "/birthdays" => "birthdays#create"
-  # patch "/birthdays/:id" => "birthdays#update"
+  patch "/birthdays/:id" => "birthdays#update"
   # delete "/birthdays/:id" => "birthdays#destroy"
 end
